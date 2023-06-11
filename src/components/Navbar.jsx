5ae1b1d6ce1b1e1cbee01efbeb817c319a19acf8
dandino4real <daniel.iwegbue@decagon.dev>
2023-06-10 18:13:38 -0700

    added navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+function NavbarComponent() {
+  return (
+    <Navbar bg="dark" expand="lg">
+      <Container className="d-flex">
+        <Navbar.Brand as={Link} to='/' className='text-white'>onlineShop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to='/' className='text-white'>Home</Nav.Link>
+            <Nav.Link as={Link} to='/cart' className='text-white'>Cart</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComponent;
