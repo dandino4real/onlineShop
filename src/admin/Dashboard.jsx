6f419dd3ab3fa5cb3d8571f94aa184ae9c326a17
dandino4real <daniel.iwegbue@decagon.dev>
2023-06-20 20:50:18 -0700

    deleted styled component

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -53,37 +53,3 @@ const Dashboard = () => {
 
 export default Dashboard;
 
-const StyledDashboard = styled.div`
-  display: flex;
-  height: 100vh;
-`;
-
-const SideNav = styled.div`
-  border-right: 1px solid gray;
-  height: calc(100vh - 70px);
-  position: fixed;
-  overflow-y: auto;
-  width: 200px;
-  display: flex;
-  flex-direction: column;
-  padding: 2rem;
-
-  h3 {
-    margin: 0 0 1rem 0;
-    padding: 0;
-    text-transform: uppercase;
-    font-size: 17px;
-  }
-
-  a {
-    text-decoration: none;
-    margin-bottom: 1rem;
-    font-size: 14px;
-  }
-`;
-
-const Content = styled.div`
-  margin-left: 200px;
-  padding: 2rem 3rem;
-  width: 100%;
-`;
